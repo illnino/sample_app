8e55c812631639bb5707808be04fd4424a6467d9
illnino ho <illnino.giggs@gmail.com>
2012-06-02 17:51:07 +0800

    Chapter 5 with Exercise

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -37,4 +37,21 @@ describe "StaticPages" do
       page.should have_selector('title', text: full_title('Contact'))
     end
   end
+
+  describe "check links" do
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About Us')
+      
+      click_link "Help"
+      page.should have_selector 'title', text: full_title('Help')
+
+      click_link "Home"
+      click_link "Sign up now!"
+      page.should have_selector 'title', text: full_title('Sign up')
+
+    end    
+  end
+
 end
